4dbbe4847b1a380398e44634a8c00f4d0f931e07
Nico Olivet <106932947+nicolasolivet@users.noreply.github.com>
2022-12-18 23:01:18 -0300

    actualice main.js

--- main.js
+++ main.js
@@ -1,299 +1,121 @@
-const productos = [
-    //* Notebooks
-    {
-        id: "notebook-01",
-        titulo: "Notebook 01",
-        imagen: "./images/notebooks/mac1.jpeg",
-        categoria: {
-            nombre: "Notebooks",
-            id: "notebooks"
-        },
-        precio: 250000
-    },
-    {
-        id: "notebook-02",
-        titulo: "Notebook 02",
-        imagen: "./images/notebooks/mac1.jpeg",
-        categoria: {
-            nombre: "Notebooks",
-            id: "notebooks"
-        },
-        precio: 250000
-    },
-    {
-        id: "notebook-03",
-        titulo: "Notebook 03",
-        imagen: "./images/notebooks/mac1.jpeg",
-        categoria: {
-            nombre: "Notebooks",
-            id: "notebooks"
-        },
-        precio: 250000
-    },
-    {
-        id: "notebook-04",
-        titulo: "Notebook 04",
-        imagen: "./images/notebooks/mac1.jpeg",
-        categoria: {
-            nombre: "Notebooks",
-            id: "notebooks"
-        },
-        precio: 250000
-    },
-    {
-        id: "notebook-05",
-        titulo: "Notebook 05",
-        imagen: "./images/notebooks/mac1.jpeg",
-        categoria: {
-            nombre: "Notebooks",
-            id: "notebooks"
-        },
-        precio: 250000
-    },
-    {
-        id:"notebook-06",
-        titulo: "Notebook 06",
-        imagen: "./images/notebooks/mac1.jpeg",
-        categoria: {
-            nombre: "Notebooks",
-            id:"notebooks"
-        },
-        precio: 250000
-    },
-    //* Celulares
-    {
-        id: "celular-01",
-        titulo: "Celular 01",
-        imagen: "./images/celulares/samsung1.png",
-        categoria: {
-            nombre: "Celulares",
-            id: "celulares"
-        },
-        precio: 60000
-    },
-    {
-        id: "celular-02",
-        titulo: "Celular 02",
-        imagen: "./images/celulares/samsung1.png",
-        categoria: {
-            nombre: "Celulares",
-            id: "celulares"
-        },
-        precio: 60000
-    },
-    {
-        id: "celular-03",
-        titulo: "Celular 03",
-        imagen: "./images/celulares/samsung1.png",
-        categoria: {
-            nombre: "Celulares",
-            id: "celulares"
-        },
-        precio: 60000
-    },
-    {
-        id: "celular-04",
-        titulo: "Celular 04",
-        imagen: "./images/celulares/samsung1.png",
-        categoria: {
-            nombre: "Celulares",
-            id: "celulares"
-        },
-        precio: 60000
-    },
-    {
-        id: "celular-05",
-        titulo: "Celular 05",
-        imagen: "./images/celulares/samsung1.png",
-        categoria: {
-            nombre: "Celulares",
-            id: "celulares"
-        },
-        precio: 60000
-    },
-    {
-        id: "celular-06",
-        titulo: "Celular 06",
-        imagen: "./images/celulares/samsung1.png",
-        categoria: {
-            nombre: "Celulares",
-            id: "celulares"
-        },
-        precio: 60000
-    },
-    {
-        id: "celular-07",
-        titulo: "Celular 07",
-        imagen: "./images/celulares/samsung1.png",
-        categoria: {
-            nombre: "Celulares",
-            id: "celulares"
-        },
-        precio: 60000
-    },
-    {
-        id: "celular-08",
-        titulo: "Celular 08",
-        imagen: "./images/celulares/samsung1.png",
-        categoria: {
-            nombre: "Celulares",
-            id: "celulares"
-        },
-        precio: 60000
-    },
-    //* Auriculares
-    {
-        id: "auricular-01",
-        titulo: "Auricular 01",
-        imagen: "./images/auriculares/auricular1.png",
-        categoria: {
-            nombre: "Auriculares",
-            id: "auriculares"
-        },
-        precio: 15000
-    },
-    {
-        id: "auricular-02",
-        titulo: "Auricular 02",
-        imagen: "./images/auriculares/auricular1.png",
-        categoria: {
-            nombre: "Auriculares",
-            id: "auriculares"
-        },
-        precio: 15000
-    },
-    {
-        id: "auricular-03",
-        titulo: "Auricular 03",
-        imagen: "./images/auriculares/auricular1.png",
-        categoria: {
-            nombre: "Auriculares",
-            id: "auriculares"
-        },
-        precio: 15000
-    },
-    {
-        id: "auricular-04",
-        titulo: "Auricular 04",
-        imagen: "./images/auriculares/auricular1.png",
-        categoria: {
-            nombre: "Auriculares",
-            id: "auriculares"
-        },
-        precio: 15000
-    },
-    {
-        id: "auricular-05",
-        titulo: "Auricular 05",
-        imagen: "./images/auriculares/auricular1.png",
-        categoria: {
-            nombre: "Auriculares",
-            id: "auriculares"
-        },
-        precio: 15000
-    },
-    {
-        id: "auricular-06",
-        titulo: "Auricular 06",
-        imagen: "./images/auriculares/auricular1.png",
-        categoria: {
-            nombre: "Auriculares",
-            id: "auriculares"
-        },
-        precio: 15000
-    }
-
-];
-
-
 const contenedorProductos = document.querySelector("#contenedor-productos");
 const botonesCategoria = document.querySelectorAll(".boton-categoria");
 const tituloPrincipal = document.querySelector("#titulo-principal");
 let botonesAgregar = document.querySelectorAll(".producto-agregar");
-const numerito = document.querySelector("#numerito");
+const contador = document.querySelector("#contador");
 
+const productos = "productos.json";
 
-function cargarProductos(productosElegidos) {
+function cargarProductos() {
 
-    contenedorProductos.innerHTML = "";
+    fetch(productos)
+        .then(respuesta => respuesta.json())
+        .then(productos => {
+            contenedorProductos.innerHTML = ""
+            productos.forEach(producto => {
 
-    productosElegidos.forEach(producto => {
-
-        const div = document.createElement("div");
-        div.classList.add("producto");
-        div.innerHTML = `
-            <img class="producto-imagen" src="${producto.imagen}" alt="${producto.titulo}">
-            <div class="producto-detalles">
+                const div = document.createElement("div");
+                div.classList.add("producto");
+                div.innerHTML = `
+                <img class="producto-imagen" src="${producto.imagen}" alt="${producto.titulo}">
+                <div class="producto-detalles">
                 <h3 class="producto-titulo">${producto.titulo}</h3>
                 <p class="producto-precio">$${producto.precio}</p>
                 <button class="producto-agregar" id="${producto.id}">Agregar</button>
-            </div>
-        `;
+                </div>
+            `;
 
-        contenedorProductos.append(div);  
-    })
+                contenedorProductos.append(div);
 
-    actualizarBotonesAgregar();
+                // actualizarBotonesAgregar();
+                const boton = document.getElementById(`${producto.id}`);
+                boton.addEventListener('click', () => {
+                    agregarAlCarrito(productos, producto.id);
+                });
+            })
+            mostrarCategoria(productos);
+        })
+    // actualizarBotonesAgregar();    
+    
 }
 
-cargarProductos(productos);
+function mostrarCategoria(productos) {
+    botonesCategoria.forEach(boton => {
+        boton.addEventListener("click", (e) => {
 
-botonesCategoria.forEach(boton => {
-    boton.addEventListener("click", (e) => {
+            botonesCategoria.forEach(boton => boton.classList.remove("active"));
+            e.currentTarget.classList.add("active");
 
-        botonesCategoria.forEach(boton => boton.classList.remove("active"));
-        e.currentTarget.classList.add("active");
+            if (e.currentTarget.id != "todos") {
+                const productoCategoria = productos.find(producto => producto.categoria.id === e.currentTarget.id);
+                tituloPrincipal.innerText = productoCategoria.categoria.nombre;
+                const productosFiltrados = productos.filter(producto => producto.categoria.id === e.currentTarget.id);
+                
+                contenedorProductos.innerHTML = ""
+                
+                productosFiltrados.forEach((producto) => {
+                    const div = document.createElement("div");
+                    div.classList.add("producto");
 
-        if (e.currentTarget.id != "todos") {
-            const productoCategoria = productos.find(producto => producto.categoria.id === e.currentTarget.id);
-            tituloPrincipal.innerText = productoCategoria.categoria.nombre;
-            const productosBtn = productos.filter(producto => producto.categoria.id === e.currentTarget.id);
-            cargarProductos(productosBtn);
-        } else {
-            tituloPrincipal.innerText = "Todos los productos";
-            cargarProductos(productos);
-        }
+                    div.innerHTML += `
+                    <img class="producto-imagen" src="${producto.imagen}" alt="${producto.titulo}">
+                    <div class="producto-detalles">
+                    <h3 class="producto-titulo">${producto.titulo}</h3>
+                    <p class="producto-precio">$${producto.precio}</p>
+                    <button class="producto-agregar" id="${producto.id}">Agregar</button>
+                    </div>
+                `;
 
-    })
-});
-
-function actualizarBotonesAgregar() {
-    botonesAgregar = document.querySelectorAll(".producto-agregar");
-
-    botonesAgregar.forEach((boton => {
-        boton.addEventListener("click", agregarAlCarrito);
-    }))
+                    contenedorProductos.append(div);
+                    
+                    
+                    const boton = document.getElementById(`${producto.id}`);
+                    boton.addEventListener('click', () => {
+                        agregarAlCarrito(productos, producto.id);
+                    });
+                })
+            
+            } else {
+                tituloPrincipal.innerText = "Todos los productos";
+                cargarProductos();
+            }
+        })
+    });
 }
+
+cargarProductos();
 
 let productosEnCarrito;
 
-let productosEnCarritoLS = localStorage.getItem("productos-en-carrito");
+let productosEnCarritoLS = localStorage.getItem("productos-en-carrito")
+
 
 if (productosEnCarritoLS) {
     productosEnCarrito = JSON.parse(productosEnCarritoLS);
-    actualizarNumerito();
+    actualizarContador();
 } else {
     productosEnCarrito = [];
 }
 
-function agregarAlCarrito(e) {
-    const idBoton = e.currentTarget.id;
-    const productoAgregado = productos.find(producto => producto.id === idBoton);
-
-    if(productosEnCarrito.some(producto => producto.id === idBoton)) {
-        const index = productosEnCarrito.findIndex(producto => producto.id === idBoton);
+function agregarAlCarrito(productos, id) {
+   
+    const productoAgregado = productos.find(producto => producto.id === id);
+    console.log(productosEnCarrito)
+    if (productosEnCarrito.some(producto => producto.id === id)) {
+        const index = productosEnCarrito.findIndex(producto => producto.id === id)
         productosEnCarrito[index].cantidad++;
     } else {
-        productoAgregado.cantidad = 1;
+        productoAgregado.cantidad = 1
         productosEnCarrito.push(productoAgregado);
     }
-
-    actualizarNumerito();
+    actualizarContador();
 
     localStorage.setItem("productos-en-carrito", JSON.stringify(productosEnCarrito));
+};
+
+function actualizarContador() {
+    let nuevoContador = productosEnCarrito.reduce((acc, producto) => acc + producto.cantidad, 0);
+    contador.innerText = nuevoContador;
 }
 
-function actualizarNumerito() {
-    let nuevoNumerito = productosEnCarrito.reduce((acc, producto) => acc + producto.cantidad, 0);
-    numerito.innerText = nuevoNumerito;
-}
+
